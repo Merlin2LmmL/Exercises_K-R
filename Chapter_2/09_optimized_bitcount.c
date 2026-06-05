9fb6a370fec23e1c2241f0bc3834fa226d6549d6
Merlin2LmmL <ortnermerlin@gmail.com>
2026-06-05 15:41:07 +0200

    Completed the last remaining Exercise that included bitwise operations - Finished 2-9 - Added additional conversion.c file in "Others" Folder t oconvert a string to any format

---
+++ Chapter_2/09_optimized_bitcount.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+
+int get_line(char line[], int lim);
+int bitcount(unsigned x);
+unsigned strtoany(char str[], int b, char charset[], int len);
+
+char BINARY[]  = "01";
+
+#define MAXLINE 80
+#define INVALID -1
+
+#define TRUE  1
+#define FALSE 0
+
+int main(void)
+{
+    int len = 1;
+    char line[MAXLINE];
+    unsigned x = INVALID;
+
+    while (len > 0) {
+        if (x == INVALID) {
+            printf("Please enter a desired value for x in binary:\n");
+            len = get_line(line, MAXLINE);
+            x = strtoany(line, 2, BINARY, len);
+            continue;
+        }
+
+        if (x != INVALID) {
+            printf("%d\n", bitcount(x));
+            x = INVALID;
+        }
+    }
+
+    return 0;
+}
+
+int get_line(char line[], int lim)
+{
+    int c, i;
+
+    for (i = 0; i < lim - 1 && (c = getchar()) != '\n' && c != EOF; ++i) {
+        line[i] = c;
+    }
+
+    line[i] = '\0';
+
+    return i;
+}
+
+int bitcount(unsigned x)
+{
+    int b;
+
+    /**
+     *  We mask x with itself with its rightmost 1 bit
+     *  deleted and delete all resulting 1 bits in the
+     *  process, resulting in couting the rightmost 1
+     *  bit each iteration.
+     */
+    for (b = 0; x != 0; ++b)
+        x &= (x - 1);
+
+    return b;
+}
+
+unsigned strtoany(char str[], int b, char charset[], int len)
+{
+    unsigned value = 0;
+
+    for (int i = 0; i < len; ++i) {
+        int found = FALSE;
+        for (int j = 0; j < b; ++j) {
+            if (str[i] == charset[j]) {
+                value *= b;
+                value += j;
+                found = TRUE;
+            }
+        }
+        if (!found) {
+            printf("Invalid base %d value!\n", b);
+            return INVALID;
+        }
+    }
+
+    return value;
+}
---
+++ Others/conversion.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+unsigned strtob(char str[], int len);
+unsigned strtoi(char str[], int len);
+
+int main(void)
+{
+    return 0;
+}
+
+unsigned strtob(char str[], int len)
+{
+    int num = 0;
+
+    for (int i = 0; i < len; ++i) {
+        for (int j = '0'; j <= '1'; ++j) {
+            if (str[i] = j) {
+                num *= 2;
+                num += j - '0';
+            }
+        }
+        return -1;
+    }
+
+    return num;
+}
+
+int strtoi(char str[], int len) {
+{
+    int num = 0;
+
+    for (int i = 0; i < len; ++i) {
+        for (int j = '0'; j <= '9'; ++j) {
+            if (str[i] = j) {
+                num *= 10;
+                num += j - '0';
+            }
+        }
+        return -1;
+    }
+
+    return num;
+}
